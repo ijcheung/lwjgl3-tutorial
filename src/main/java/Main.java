b3077d0c2d83b52c86beba0e6a5cb2dc7e6fe1c9
Ivan Cheung <ivan.j.cheung@gmail.com>
2017-02-19 20:42:50 -0600

    refactor: Lwjgl3Test.java -> Main.java
    feat: first quad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,127 @@
-import org.lwjgl.glfw.GLFWvidmode;
-import org.lwjgl.opengl.GL11;
-
-import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-
-/**
- * Created by tachyonflux on 2/19/2017.
- */
-
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+ 
+import java.nio.ByteBuffer;
+ 
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+ 
 public class Main {
-    public static void main(String[] args) {
-        SharedLibraryLoader.load();
-        if(glfwInit() == GL11.GL_FALSE) {
-            throw new IllegalStateException("Failed to initialize GLFW");
+ 
+    // We need to strongly reference callback instances.
+    private GLFWErrorCallback errorCallback;
+    private GLFWKeyCallback   keyCallback;
+ 
+    // The window handle
+    private long window;
+ 
+    public void run() {
+        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+ 
+        try {
+            init();
+            loop();
+ 
+            // Release window and window callbacks
+            glfwDestroyWindow(window);
+            keyCallback.release();
+        } finally {
+            // Terminate GLFW and release the GLFWerrorfun
+            glfwTerminate();
+            errorCallback.release();
         }
-        glfwWindowHint(GLFW_VISIBLE, GL11.GL_FALSE);
-        long window = glfwCreateWindow(640, 480, "MY LWJGL Program", 0, 0);
-        if(window == 0) {
-            throw new IllegalStateException("Failed to create window");
-        }
-
-        GLFWvidmode videoMode = new GLFWvidmode(glfwGetVideoMode(glfwGetPrimaryMonitor()));
-        glfwSetWindowPos(window, (videoMode.getWidth() - 640) / 2, (videoMode.getHeight() - 480)/ 2);
-
+    }
+ 
+    private void init() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+ 
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( glfwInit() != GL11.GL_TRUE )
+            throw new IllegalStateException("Unable to initialize GLFW");
+ 
+        // Configure our window
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+ 
+        int WIDTH = 300;
+        int HEIGHT = 300;
+ 
+        // Create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+ 
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+            }
+        });
+ 
+        // Get the resolution of the primary monitor
+        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        // Center our window
+        glfwSetWindowPos(
+            window,
+            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+        );
+ 
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+ 
+        // Make the window visible
         glfwShowWindow(window);
+    }
+ 
+    private void loop() {
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the ContextCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GLContext.createFromCurrent();
+ 
+        // Set the clear color
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+ 
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-        while(glfwWindowShouldClose(window) == GL11.GL_FALSE) {
+            glBegin(GL11.GL_QUADS);
+            glColor4f(1, 0, 0, 1);
+            glVertex2f(-0.5f, 0.5f);
+            glColor4f(0, 1, 0, 1);
+            glVertex2f(0.5f, 0.5f);
+            glColor4f(0, 0, 1, 1);
+            glVertex2f(0.5f, -0.5f);
+            glColor4f(1, 1, 1, 1);
+            glVertex2f(-0.5f, -0.5f);
+            glEnd();
+
+            glfwSwapBuffers(window); // swap the color buffers
+ 
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
             glfwPollEvents();
         }
-
-        glfwTerminate();
     }
+ 
+    public static void main(String[] args) {
+    	SharedLibraryLoader.load();
+        new Main().run();
+    }
+ 
 }
