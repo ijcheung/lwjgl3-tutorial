7ba8ddc4842db40e66d1d2775ecb79d83b1200ea
Ivan Cheung <ivan.j.cheung@gmail.com>
2017-02-19 20:58:49 -0600

    feat: simple keyboard input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,50 @@
 import org.lwjgl.Sys;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
- 
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWvidmode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
+
 import java.nio.ByteBuffer;
- 
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.*;
+
+import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_A;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetKey;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glColor4f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glVertex2f;
+import static org.lwjgl.system.MemoryUtil.NULL;
  
 public class Main {
  
@@ -94,25 +131,29 @@ public class Main {
  
         // Set the clear color
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
- 
+
+        boolean a_press;
+
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+            a_press = glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS;
+
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-            glBegin(GL11.GL_QUADS);
-            glColor4f(1, 0, 0, 1);
+            glBegin(GL_QUADS);
+            glColor4f(a_press?0:1, a_press?1:0, 0, 1);
             glVertex2f(-0.5f, 0.5f);
-            glColor4f(0, 1, 0, 1);
+            glColor4f(a_press?1:0, a_press?0:1, 0, 1);
             glVertex2f(0.5f, 0.5f);
-            glColor4f(0, 0, 1, 1);
+            glColor4f(a_press?1:0, a_press?1:0, 1, 1);
             glVertex2f(0.5f, -0.5f);
-            glColor4f(1, 1, 1, 1);
+            glColor4f(a_press?0:1, a_press?0:1, 1, 1);
             glVertex2f(-0.5f, -0.5f);
             glEnd();
 
             glfwSwapBuffers(window); // swap the color buffers
- 
+
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents();
